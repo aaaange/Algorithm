22f10461b6aedf3f71851ab1f4264b758a9a54c0
aaange <ayung00@naver.com>
2025-02-24 08:55:31 +0900

    feat(1159.js) 농구 게임 JavaScript - 2차 제출 출력초과

--- Baekjoon/Bronze/1159_농구_게임/1159.js
+++ Baekjoon/Bronze/1159_농구_게임/1159.js
@@ -1,13 +1,22 @@
+`
+- 분류 : 구현, 문자열
+- 성능 요약
+    - 메모리 : 9592 KB
+    - 시간 : 96 ms
+- 제출 일자 : 2025-01-22 17:52
+`
+
 const fs = require("fs"); 
-// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const input = fs.readFileSync("input.txt").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// const input = fs.readFileSync("input.txt").toString().trim().split("\n");
 
 const N = parseInt(input[0])
 
 const member = []
 
 for (let i = 1; i < N + 1 ; i ++) {
-    member.add(input[i])
+    // member.add(input[i]) // add는 배열(Array)의 메서드가 아닌 set의 메서드
+    member.push(input[i])
 }
 
 console.log(member)
