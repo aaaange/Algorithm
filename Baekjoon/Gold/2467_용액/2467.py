ecada2625d2eeef3070c3f4681578a1beed28c64
aaange <ayung00@naver.com>
2025-02-24 17:12:08 +0900

    feat(2467.py) 2467 용액 - 2차 제출 오답

--- Baekjoon/Gold/2467_용액/2467.py
+++ Baekjoon/Gold/2467_용액/2467.py
@@ -1,15 +1,23 @@
 N = int(input()) # 전체 용액의 수
 arr = list(map(int, input().split()))
-re_arr = sorted(arr, reverse=True)
 
-min_value = re_arr[0]
+min_value = float("inf")
 min_list = []
 current_value = 0
 
-for i in range(N//2+1):
-    current_value = arr[i] + re_arr[i]
+i = 0
+j = N-1
+while i <= (N//2+1) and j >= (N//2-1) and arr[i] != arr[j]:
+    current_value = arr[i] + arr[j]
     if min_value > abs(current_value):
-        min_value = current_value
-        min_list = [arr[i], re_arr[i]]
+        min_value = abs(current_value)
+        min_list = [arr[i], arr[j]]
+    else:
+        break
+    if 0 < current_value:
+        j -= 1
+    elif 0 > current_value:
+        i += 1
+        
 
 print(min_list)
